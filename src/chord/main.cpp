0b21baf1fe2742a4eac3c22d5a6bee12351a3d07
Sagarsakta555 <ssakta@msystechologies.com>
2023-01-02 17:01:52 +0530

    rebasing the main.cpp file

--- src/chord/main.cpp
+++ src/chord/main.cpp
@@ -15,57 +15,57 @@ CommandLine * gCommandLine = nullptr;
 
 int32 main(int32 argc, char ** argv)
 {
-	//////////////////////////////////////////////////
-	// Application setup
-	//////////////////////////////////////////////////
+    //////////////////////////////////////////////////
+    // Application setup
+    //////////////////////////////////////////////////
 
-	// Create globals    
-	Memory::createGMalloc();
-	gThreadManager = new ThreadManager();
-	gCommandLine = new CommandLine(argc, argv);
+    // Create globals    
+    Memory::createGMalloc();
+    gThreadManager = new ThreadManager();
+    gCommandLine = new CommandLine(argc, argv);
+    
+    Chord::LocalNode localNode;
 
-	Chord::LocalNode localNode;
+    Net::Ipv4 peer;
+    if (CommandLine::get().getValue("input", peer, [](const String & str, Net::Ipv4 & peer){
 
-	Net::Ipv4 peer;
-	if (CommandLine::get().getValue("input", peer, [](const String & str, Net::Ipv4 & peer){
+        Net::parseIpString(peer, *str);
+    })) localNode.join(peer);
 
-				Net::parseIpString(peer, *str);
-				})) localNode.join(peer);
+    auto receiver = RunnableThread::create(new Chord::ReceiveTask(&localNode), "Receiver");
+    auto updater = RunnableThread::create(new Chord::UpdateTask(&localNode), "Updater");
 
-	auto receiver = RunnableThread::create(new Chord::ReceiveTask(&localNode), "Receiver");
-	auto updater = RunnableThread::create(new Chord::UpdateTask(&localNode), "Updater");
+    char line[256] = {};
+    char c; do
+    {
+        switch (c = getc(stdin))
+        {
+        case 'p':
+        {
+            localNode.printInfo();
+            break;
+        }
 
-	char line[256] = {};
-	char c; do
-	{
-		switch (c = getc(stdin))
-		{
-			case 'p':
-				{
-					localNode.printInfo();
-					break;
-				}
+        case 'l':
+        {
+            uint32 key;
+            scanf("%x", &key);
 
-			case 'l':
-				{
-					uint32 key;
-					scanf("%x", &key);
+            auto peer = localNode.lookup(key);
+            printf("RESULT: found key 0x%08x @ [%s]\n", key, *peer.get().getInfoString());
+            break;
+        }
+        
+        case 'q':
+        {
+            localNode.leave();
+            break;
+        }
 
-					auto peer = localNode.lookup(key);
-					printf("RESULT: found key 0x%08x @ [%s]\n", key, *peer.get().getInfoString());
-					break;
-				}
+        default:
+            break;
+        }
+    } while(c != 'q');
 
-			case 'q':
-				{
-					localNode.leave();
-					break;
-				}
-
-			default:
-				break;
-		}
-	} while(c != 'q');
-
-	return 0;
+    return 0;
 }
